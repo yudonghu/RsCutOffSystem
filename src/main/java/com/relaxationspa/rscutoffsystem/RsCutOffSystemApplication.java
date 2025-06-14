0c37bee680398f31818d57ce9721a1226d2bd114
yudonghu <hydcpp918@gmail.com>
2025-06-14 14:13:28 -0700

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/relaxationspa/rscutoffsystem/RsCutOffSystemApplication.java
@@ -0,0 +1,13 @@
+package com.relaxationspa.rscutoffsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsCutOffSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsCutOffSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/relaxationspa/rscutoffsystem/RsCutOffSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relaxationspa.rscutoffsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsCutOffSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
